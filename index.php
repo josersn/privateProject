81368bff0b98fd8b57b08ca9f4edc7709dbab73f
José ramos <nettorammos@hotmail.com>
2019-11-04 18:17:10 -0300

    :arrow_up: Arquivos de inicialização do projeto

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include_once('config/variables.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH?>style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d234afb51c.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <i class="far fa-moon fa-2x"></i>
+            <h1>Astrale</h1>
+        </div>
+        <nav class="nav">
+            <ul>
+                <li>Blog</li>
+                <li>Mapa Astral</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    </header>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</body>
+</html>
